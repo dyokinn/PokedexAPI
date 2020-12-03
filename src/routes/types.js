9b2d59931ee66a30cff38c148049d3935d589333
Di-santos <dio.silveira07@gmail.com>
2020-12-03 16:41:17 -0300

    v 0.7.0
    - Implementação da rota Types:{
           - Rota Padrão
           - Busca

---
+++ src/routes/types.js
@@ -0,0 +1,16 @@
+// Import no Express e no Router para módulos de rotas
+var express = require('express');
+var router = express.Router();
+
+const data = require('../database/pokedex.json')
+
+// Listar a Pokedex de Pokémons que possuem um tipo específico
+router.get('/:type', function (req, res) {
+
+    const {type} = req.params
+    const filtrados = data.filter(pokemon => pokemon.type.includes(type))
+    res.json(filtrados)
+})
+
+// Export no módulo com a rota principal e suas secundárias
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -5,15 +5,17 @@ const app = express()
 // Imports nas Rotas
 const pokedex = require('./routes/pokedex');
 const regions = require('./routes/regions');
+const types = require('./routes/types');
 
 
 // Compatibilidade JSON e uso das rotas
 app.use(express.json())
 app.use('/pokedex', pokedex);
 app.use('/regions', regions);
+app.use('/types', types);
 
 
-// Porta 3000
+// Atribuição da porta 3000
 app.listen(3000, ()=>{
     console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n +++ Server Online +++ \n\n\n')
 })
