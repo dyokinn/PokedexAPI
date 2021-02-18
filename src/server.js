24d50db41a30121a49a26dba4ad882fa44efdc06
Di-santos <dio.silveira07@gmail.com>
2021-02-17 23:29:59 -0300

    v 0.8.0
    - Implementação do CORS (para integração API)

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // Imports no Express e Instanciação do App
 import express from "express";
+import cors from "cors";
 const app = express()
 
 // Imports nas Rotas
@@ -10,6 +11,7 @@ const types = require('./routes/types');
 
 // Compatibilidade JSON e uso das rotas
 app.use(express.json())
+app.use(cors())
 app.use('/search', search);
 app.use('/regions', regions);
 app.use('/types', types);
